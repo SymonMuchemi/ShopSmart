ae8fb457e40ee689d357dae54191abca21d31cd8
symonmuchemi <simonkenya846@gmail.com>
2024-12-27 00:32:53 +0300

    refactor(types): move user, product, cart, and response type definitions to seperate files

---
+++ src/types/models.types.ts
@@ -0,0 +1,37 @@
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+    username: string;
+    password: string;
+    email: string;
+    role: string | 'user' | 'admin';
+}
+
+export interface IProduct extends Document {
+    name: string;
+    description: string;
+    price: number;
+    quantity: number;
+    imageUrls: [string];
+    discount?: number;
+    videoUrl?: string;
+    category: string;
+}
+
+export interface ICartItem extends Document {
+    product: string;
+    name: string;
+    quantity: number;
+    price: number;
+    total_amount: number;
+}
+
+export interface ICart extends Document {
+    user: string;
+    cartITems: [string];
+    total_items: number;
+    total_amount: number;
+}
+
+export type AuthUser = Pick<IUser, 'email' | 'password'>
+export type UpdateProduct = Partial<IProduct>;
---
+++ src/types/response.type.ts
@@ -0,0 +1,5 @@
+export interface ReturnResponse {
+    code: number;
+    message: string;
+    details: any;
+}
