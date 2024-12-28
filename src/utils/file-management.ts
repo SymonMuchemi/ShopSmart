c4cbaeceadaf35ec7d8cb27d26dfbc4545e268b6
symonmuchemi <simonkenya846@gmail.com>
2024-12-28 21:19:43 +0300

    feat: add file management utilities for generating file names and resizing images

---
+++ src/utils/file-management.ts
@@ -0,0 +1,21 @@
+import crypto from 'crypto';
+import sharp from 'sharp';
+
+export const genereteFileName = (bytes = 32) => {
+    return crypto.randomBytes(bytes).toString('hex');
+}
+
+export const resizeImage = async (file: Express.Multer.File) => {
+    try {
+        const buffer = file.buffer;
+
+        const resizedBuffer = await sharp(buffer)
+            .resize({ height: 1920, width: 1080 })
+            .toBuffer()
+
+        return resizedBuffer;
+
+    } catch (error: any) {
+        throw new Error(`Error resizing image: ${error.toString()}`);
+    }
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import { asyncHandler, handleRequest } from "./methods";
 import { IMAGE_VIDEO_URL_REGEX } from "./constants";
+import { genereteFileName, resizeImage } from './file-management'
 
-export { asyncHandler, handleRequest, IMAGE_VIDEO_URL_REGEX }
+export { asyncHandler, handleRequest, IMAGE_VIDEO_URL_REGEX, genereteFileName, resizeImage }
