bac824c22f816a0062b6cfd0db09d4f5062972ba
symonmuchemi <simonkenya846@gmail.com>
2025-01-15 00:04:03 +0300

    update: make purchase model available and maintains consisency in item exports

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -2,6 +2,7 @@ import Product from "./Product";
 import Cart from "./Cart";
 import CartITem from "./CartITem";
 import User from "./User";
+import Purchase from "./Purchase";
 
 
-export { Product, Cart, CartITem, User };
+export { Product, Cart, CartITem, User, Purchase };
