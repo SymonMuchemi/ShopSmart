f1cc14e7d9a2fc7b4fdb7e0aa328bb4d721d2960
symonmuchemi <simonkenya846@gmail.com>
2025-01-25 15:06:01 +0300

    update:  API routes updated to include versioning for better organization

--- src/server.ts
+++ src/server.ts
@@ -24,11 +24,11 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ limit: "500kb", extended: true }));
 app.use(cookieParser(process.env.JWT_SECRET));
 
-app.use('/auth', authRouter);
-app.use('/cart', cartRouter);
-app.use('/products', productsRouter);
-app.use('/purchase', purchaseRouter);
-app.use('/', paymentRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/cart', cartRouter);
+app.use('/api/v1/products', productsRouter);
+app.use('/api/v1/purchase', purchaseRouter);
+app.use('/api/v1/', paymentRouter);
 
 // simple route
 app.get('/hello-world', (req: Request, res: Response) => {
