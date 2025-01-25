5bd8644e913358a30f663a2d9d126f9c3fcc6097
symonmuchemi <simonkenya846@gmail.com>
2025-01-26 00:11:33 +0300

    feat: enhance payment intent creation to return client secret and improve error handling

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -1,17 +1,19 @@
 import { createIntent } from "../stripe/utils";
 
-export const makeIntent = async (amount: number ) => {
+export const makeIntent = async (amount: number) => {
     try {
         const purchaseIntent = await createIntent(amount);
 
         return {
             code: 200,
             message: 'Purchase intent made',
-            details: purchaseIntent
+            details: {
+                clientSecret: purchaseIntent.client_secret
+            }
         }
     } catch (error: any) {
         return {
-            code: 500, 
+            code: 500,
             message: "Failed to create purchase intent",
             details: error.message
         }
--- src/stripe/utils.ts
+++ src/stripe/utils.ts
@@ -3,5 +3,7 @@ import { stripe } from './config';
 export const createIntent = async (amount: number, currency: string = 'usd') => {
     const paymentIntent = await stripe.paymentIntents.create({
         amount, currency
-    })
+    });
+
+    return paymentIntent;
 }
