00afc58c2945177ec676f70e3d5df41b3ee9f0cc
symonmuchemi <simonkenya846@gmail.com>
2025-02-24 20:37:32 +0300

    feat: integrate AWS Secrets Manager to fetch MongoDB URI and update connection logic

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -1,16 +1,41 @@
-import { color } from 'console-log-colors';
 import mongoose from 'mongoose';
+import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
+
+import { color } from 'console-log-colors';
+
+const secret_name = "shopsmart-secret-values";
+
+const client = new SecretsManagerClient({ region: 'eu-north-1' });
+
+let response;
+
 
 export const connectDB = async () => {
     try {
-        const DBUrl: string | undefined = process.env.MONGO_DB_URL;
+        // fetch MongoDB URI from AWS Secrets Manager
+        response = await client.send(
+            new GetSecretValueCommand({
+                SecretId: secret_name,
+                VersionStage: 'AWSCURRENT'
+            })
+        );
 
-        if (DBUrl === undefined) {
-            console.log(color.red.inverse('Invalid db URl'));
-            process.exit(1)
+        const secret = response.SecretString;
+
+        console.log(color.yellowBright(secret))
+
+        if (!secret) {
+            console.log(color.red.inverse("SecretString is empty!!!"));
+            process.exit(1);
         }
 
-        const conn = await mongoose.connect(DBUrl);
+        const { MONGO_DB_URL } = JSON.parse(secret);
+
+        if (!MONGO_DB_URL) {
+            console.log(color.red.inverse('MONGO DB URL NOT FOUND IN SECRET!!!'))
+        }
+
+        const conn = await mongoose.connect(MONGO_DB_URL);
 
         console.log(color.blueBright(`MongoDB connected: ${conn.connection.host}`));
     } catch (error: any) {
